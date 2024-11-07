284b93b52d61dc646d68193511d330ca824b4c8a
Yushank <yushank2001@gmail.com>
2024-11-07 19:22:21 +0530

    Added authMIddleware for user authentification

--- app/middleware/user.ts
+++ app/middleware/user.ts
@@ -1,6 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 
 
-export async function authMiddleware(){
-    
+export function authMiddleware(req: NextRequest){
+    try{
+        const token = req.cookies.get("token")?.value;
+
+        if(!token){
+            return NextResponse.redirect(new URL('/signin', req.url));
+        }
+
+        const decode = jwt.verify(token, process.env.JWT_SECRET as string)as JwtPayload
+
+        req.headers.set('userId', decode.id)
+
+        return NextResponse.next();
+    }
+    catch(error){
+        return NextResponse.redirect(new URL('/signin', req.url));
+    }
 }
